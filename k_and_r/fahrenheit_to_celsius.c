2f5ced478724de5c214d60e30d3e96af6d9b6575
Greg Larsen <gslarsen2718@gmail.com>
2025-01-29 10:35:39 -0500

    add k&r files

---
+++ k_and_r/fahrenheit_to_celsius.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  float fahr = 0;
+  int upper = 300;
+  int step = 20;
+
+  printf("Fahrenheit\tCelsius\n");
+  while (fahr <= upper)
+  {
+    float celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f\t\t%0.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
