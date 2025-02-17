4772619cce06b2c067df8657d4ea0e0ff7016bb6
Greg Larsen <gslarsen2718@gmail.com>
2025-02-17 15:18:15 -0500

    listing 1.2 - a bad example of listing 1.1

--- modernC-jens-gustedt/bad.c
+++ modernC-jens-gustedt/bad.c
@@ -1,23 +1,21 @@
 /* This may look like nonsense, but really is -*- mode: C -*- */
-
+#include <stdio.h>
 /* The main thing that this program does. */
-void main() {
+int main() {
   // Declarations
-  int i;
   double A[5] = {
-    9.0,
-    2.9,
-    3.E+25,
-    .00007,
+      9.0,
+      2.9,
+      3.E+25,
+      .00007,
   };
 
   // Doing some work
-  for (i = 0; i < 5; ++i) {
-     printf("element %d is %g, \tits square is %g\n", /*@\label{printf-start-badly}*/
-            i,
-            A[i],
-            A[i]*A[i]);                               /*@\label{printf-end-badly}*/
+  for (size_t i = 0; i < 5; ++i) {
+    printf(
+        "element %zu is %g, \tits square is %g\n", /*@\label{printf-start-badly}*/
+        i, A[i], A[i] * A[i]);                     /*@\label{printf-end-badly}*/
   }
 
-  return 0;                                           /*@\label{main-return-badly}*/
+  return 0; /*@\label{main-return-badly}*/
 }
