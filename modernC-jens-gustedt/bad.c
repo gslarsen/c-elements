2be9712c55494b131fc031fad036e2ee39b544a0
Greg Larsen <gslarsen2718@gmail.com>
2025-02-24 15:34:23 -0500

    slight modifications

--- modernC-jens-gustedt/bad.c
+++ modernC-jens-gustedt/bad.c
@@ -1,4 +1,5 @@
 /* This may look like nonsense, but really is -*- mode: C -*- */
+#include <limits.h>
 #include <stdio.h>
 /* The main thing that this program does. */
 int main() {
@@ -9,6 +10,14 @@ int main() {
       3.E+25,
       .00007,
   };
+
+  unsigned long long int u_long_max = ULLONG_MAX;
+  long long int long_long_max = LLONG_MAX;
+  long long int long_long_min = LLONG_MIN;
+
+  printf("Maximum value of unsigned long int: %llu\n", u_long_max);
+  printf("Maximum value of long long int: %lld\n", long_long_max);
+  printf("Minimum value of long long int: %lld\n", long_long_min);
 
   // Doing some work
   for (size_t i = 0; i < 5; ++i) {
