47f117248b9c7add25a34955f1eb5931a041b17e
Greg Larsen <gslarsen2718@gmail.com>
2025-05-21 23:07:21 -0400

    amend comments in  day-before-n-days.c

--- udemy-g4g/operators/day-before-n-days.c
+++ udemy-g4g/operators/day-before-n-days.c
@@ -10,13 +10,13 @@ enum { SUNDAY = 0, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY };
 
 int main(int argc, char *argv[]) {
   int d = SATURDAY, n = -1;
-  // my answer
+  // my answer; correct for all n
   int ans = (d - n) % 7 < 0 ? (d - n) % 7 + 7 : (d - n) % 7;
   // video answer; incorrect for n < 0 (e.g., n = -1)
   int ans2 = d - n % 7 < 0 ? (d - n % 7) + 7 : d - n % 7;
 
   printf("%d days before %d is %d\n", n, d, ans);
   printf("%d days before %d is %d\n", n, d, ans2);
-  printf("%d\n", -7 % 7);
+  // printf("%d\n", -7 % 7); // prints 0
   return 0;
 }
