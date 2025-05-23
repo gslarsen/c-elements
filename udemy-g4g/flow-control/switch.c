26ed6acb7e6373489ca0488ae362f4c846bf479b
Greg Larsen <gslarsen2718@gmail.com>
2025-05-23 12:00:22 -0400

    more buffering and switch stmt example

---
+++ udemy-g4g/flow-control/switch.c
@@ -0,0 +1,44 @@
+/**
+ * @file switch.c
+ * @author Gregory Larsen
+ * @date 2025-05-23
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int x = 0, y = 0, c;  // c is for clearing the input buffer
+  char ch;
+
+  printf("Enter a choice (u (up), d (down), l (left), r (right) OR x (exit)): ");
+  ch = tolower(getchar());
+  // Clear the input buffer
+  while ((c = getchar()) != '\n' && c != EOF);
+
+  while (ch != 'x') {
+    switch (ch) {
+      case 'u':
+        y++;
+        break;
+      case 'd':
+        y--;
+        break;
+      case 'l':
+        x--;
+        break;
+      case 'r':
+        x++;
+        break;
+      default:
+        printf("Invalid choice\n");
+    }
+    printf("Enter a choice (u (up), d (down), l (left), r (right) OR x (exit)): ");
+    ch = tolower(getchar());
+    // Clear the input buffer
+    while ((c = getchar()) != '\n' && c != EOF);
+  }
+
+  printf("x = %d, y = %d\nExiting...\n", x, y);
+  return 0;
+}
--- udemy-g4g/input-output/buffering.c
+++ udemy-g4g/input-output/buffering.c
@@ -10,24 +10,22 @@ int main() {
   // the program will then print the second prompt and NOT wait for another input.
   // This is because the second integer from the first input is still in the buffer!
 
-  int x, y;
-  int c;
+  int x, y, c;  // c is for clearing the input buffer
+  int ch;
 
   printf("Enter an integer: ");
   scanf("%d", &x);
   // this section shows how to handle the buffer
   // Check if the next character in the buffer is a newline
-  c = getchar();
-  if (c == '\n') {
+  ch = getchar();
+  if (ch == '\n') {
     // If newline is the next character, the buffer is empty (user entered just one number)
     printf("Enter second integer: ");
-  } else if (c != EOF) {
+  } else if (ch != EOF) {
     // Put the character back into the buffer if it's not EOF
     // ungetc(c, stdin);
     // OR, Clear the buffer up to the next newline
-    while (c != '\n' && c != EOF) {
-      c = getchar();
-    }
+    while ((c = getchar()) != '\n' && c != EOF);
     printf("Enter second integer: ");
   }
   // Now we can safely read the second integer
